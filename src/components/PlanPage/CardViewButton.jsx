5d76c767695bdedec79efe4f3959077752dbb5c6
Natarajan <983681@cognizant.com>
2026-07-14 10:17:47 -0500

    Cleaned up CardViewButton styling and fixed code consistency through TextAssest.

--- src/components/PlanPage/CardViewButton.jsx
+++ src/components/PlanPage/CardViewButton.jsx
@@ -1,12 +1,14 @@
-import { Grid2x2 } from "lucide-react"
+import { Grid2x2, Rows3 } from "lucide-react"
+import TextAsset from "../../assets/TextAssets.json"
 
 function CardViewButton ({ cardView, setCardView }) {
     return (
         <button 
             onClick={() => {setCardView(!cardView)}}
-            className="border p-2 rounded-lg"
+            title={cardView ? TextAsset.CardViewButton.list : TextAsset.CardViewButton.grid}
+            className="border p-2 rounded-lg outline-none hover:border-gray-400 focus:border-gray-400"
         >
-            <Grid2x2 size={20} />
+            {cardView ? <Rows3 size={20} /> : <Grid2x2 size={20} />}
         </button>
     )
 }

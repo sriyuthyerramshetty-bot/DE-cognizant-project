99e51d1638c1c1c68fdbdebd188534326a6c4d91
Yerramshetty <983679@cognizant.com>
2026-07-15 14:00:08 -0500

    Small changes with todo and customers

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -268,6 +268,7 @@ function Todo() {
                     disabled={isAnimatingOut || task.isCheckoutTask}
                     className="ml-2 flex h-6 w-6 shrink-0 items-center justify-center rounded-full border border-gray-300 bg-white text-black"
                     aria-label={`Complete ${task.name || 'task'}`}
+                    title={task.isCheckoutTask ? 'Completed checkout tasks cannot be altered' : null}
                   >
                     {isAnimatingOut || (task.isCompleted) ? <Check size={14} strokeWidth={3} /> : null}
                   </button>

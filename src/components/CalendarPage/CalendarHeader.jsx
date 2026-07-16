2fa6fe9ac8f42e25f9b2c77c7df394688ff6eaa8
Natarajan <983681@cognizant.com>
2026-07-16 12:17:26 -0500

    Plan Page fixes.

--- src/components/CalendarPage/CalendarHeader.jsx
+++ src/components/CalendarPage/CalendarHeader.jsx
@@ -1,6 +1,6 @@
 function CalendarHeader() {
     return (
-        <div className="grid grid-cols-7 items-center border relative rounded-md p-4 shadow-sm bg-red-300">
+        <div className="grid grid-cols-7 items-center border relative rounded-md p-4 shadow-sm border-gray-300">
             <h3 className="text-black text-lg font-semibold text-center">Sun</h3>
             <h3 className="text-black text-lg font-semibold text-center">Mon</h3>
             <h3 className="text-black text-lg font-semibold text-center">Tue</h3>
